bb6156d37e016c0401a7277dc1c2b170e39442cc
GogaBurak <maestro.prokrastinator@gmail.com>
2023-06-22 14:36:14 +0300

    fix: send 403 Forbidden status code if user_id in token doesn't exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
       decoded = JsonWebToken.decode(header)
       @current_user = User.find(decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
+      render json: { errors: e.message }, status: :forbidden
     rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
     end
