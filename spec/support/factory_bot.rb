0e8570d9caa339643bea1f44f2b64a3b3b7c9532
GogaBurak <maestro.prokrastinator@gmail.com>
2023-06-30 09:05:35 +0300

    style: use nested module definitions

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -4,11 +4,15 @@ require 'factory_bot'
 require 'pry'
 
 # add custom method for generating controller params
-module FactoryBot::Syntax::Methods
-  def params_for(model_name, options = {})
-    {
-      model_name => attributes_for(model_name, options)
-    }
+module FactoryBot
+  module Syntax
+    module Methods
+      def params_for(model_name, options = {})
+        {
+          model_name => attributes_for(model_name, options)
+        }
+      end
+    end
   end
 end
 
